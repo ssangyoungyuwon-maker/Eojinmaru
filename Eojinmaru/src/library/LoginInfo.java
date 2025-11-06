f327e80cecad6f5a92fc98a01f6375c769f6e2c9
user <user@DESKTOP-MGVGOQ0>
2025-11-06 16:58:36 +0900

    김초원 userUI 수정

--- Eojinmaru/src/library/LoginInfo.java
+++ Eojinmaru/src/library/LoginInfo.java
@@ -1,16 +1,18 @@
 package library;
 
 public class LoginInfo {
-private MainDTO loginUser = null;
+	private MainDTO loginUser = null;
 
-public MainDTO loginUser() {
-	return loginUser;
-}
-public void login(MainDTO loginUser) {
-	this.loginUser = loginUser;
-}
-public void logout() {
-	loginUser = null;
-}
+	public MainDTO loginUser() {
+		return loginUser;
+	}
+
+	public void login(MainDTO loginUser) {
+		this.loginUser = loginUser;
+	}
+
+	public void logout() {
+		loginUser = null;
+	}
 
 }
--- Eojinmaru/src/library/UserUI.java
+++ Eojinmaru/src/library/UserUI.java
@@ -3,24 +3,31 @@ package library;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import DBUtil.DBConn;
 
 public class UserUI {
 	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	// private UserDAO dao = new UserDAOImpl();
+	private LoginInfo login = null;
+	
+	public UserUI(LoginInfo login) {
+		this.login = login;
+	}
 	
 	public void menu() {
+	
 		int ch = 0;
 		
 		while(true) {
 			System.out.println("\n[사용자 화면]");
+			
 			try {
 				System.out.print("1.도서검색 2.대출신청/연장 3.반납신청 4.도서신청 5.마이페이지 6.로그아웃");
 				ch = Integer.parseInt(br.readLine());
 				
 				if(ch == 6) {
-					DBConn.close();
+					login.logout();
 					System.out.println("로그아웃 되었습니다.");
+					
 					return;
 				}
 					switch(ch) {
@@ -37,22 +44,27 @@ public class UserUI {
 	}
 	// 1. 도서 검색
 	protected void findBybook() {
-		
+		System.out.println("\n[도서검색]");
+
 	}
 	// 2. 대출신청/연장
 	protected void loan () {
+		System.out.println("\n[대출신청 / 연장]");
 		
 	}
 	// 3. 반납신청
 	protected void returnbook() {
+		System.out.println("\n[반납신청]");
 		
 	}
 	// 4. 도서신청
 	protected void sincheong() {
+		System.out.println("\n[도서신청]");
 		
 	}
 	// 5. 마이페이지
 	protected void mypage() {
+		System.out.println("\n[마이페이지]");
 		
 	}
 }
