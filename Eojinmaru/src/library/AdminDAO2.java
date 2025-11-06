cc15bca703c36cd345a5e575b2233cf0f8aec7ac
user1 <user1@user>
2025-11-07 08:43:29 +0900

    서유원 AdminDAO2, DAOImpl2 생성

--- Eojinmaru/src/library/AdminDAO2.java
+++ Eojinmaru/src/library/AdminDAO2.java
@@ -1,13 +1,21 @@
 package library;
 
+import java.util.List;
+
 
 public interface AdminDAO2 {
 	
-	public static void loan_book_search() {
-		
-	}
 	
+	public void LoanBookSearchByPeriod(String loanperiod) ;
+
+	public void LoanBookSearchByName(String bookname) ;
 	
+	public boolean LoanBookbaega() ;
 	
+	public List<AdminDTO2> LoanBookList();
+	
+	public List<AdminDTO2> returnBookList() ;
+	
+	public List<AdminDTO2> overdueBookList() ;
 	
 }
--- Eojinmaru/src/library/AdminDAOImpl2.java
+++ Eojinmaru/src/library/AdminDAOImpl2.java
@@ -1,12 +1,43 @@
 package library;
 
 import java.sql.Connection;
+import java.util.List;
 
 import DBUtil.DBConn;
 
-public class AdminDAOImpl2  {
-	
+public class AdminDAOImpl2  implements AdminDAO2 {
 	private Connection conn = DBConn.getConnection();
+
+	@Override
+	public void LoanBookSearchByPeriod(String loanperiod) {
+		
+	}
+
+	@Override
+	public void LoanBookSearchByName(String bookname) {
+		
+	}
+
+	@Override
+	public boolean LoanBookbaega() {
+		return false;
+	}
+
+	@Override
+	public List<AdminDTO2> LoanBookList() {
+		return null;
+	}
+
+	@Override
+	public List<AdminDTO2> returnBookList() {
+		return null;
+	}
+
+	@Override
+	public List<AdminDTO2> overdueBookList() {
+		return null;
+	}
+	
 
     
     
